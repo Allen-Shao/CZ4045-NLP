753678ed65f6543d6e48b936d952d6dbe9cca41f
Allen-Shao <shaoyiyang312@hotmail.com>
2017-10-11 02:47:43 +0800

    help funcs

--- help_func.py
+++ help_func.py
@@ -2,10 +2,15 @@ import pandas as pd
 
 def read_dataframe(path):
 	return pd.read_csv(path, index_col=0)
+	# TODO: Need some process maybe
 
 
 def find_answers(answers_df, questionId):
 	return answers_df.loc[(answers_df["ParentId"] == questionId)]
+
+
+def find_answers(questions_df, answers_df, questionId):
+	return answers_df.loc[answers_df["PostId"] == questions_df.loc[questions_df['PostId']==questionId]['AcceptedAnswerId']]
 
 
 def check_answers_count(questions_df, answers_df, questionId):
