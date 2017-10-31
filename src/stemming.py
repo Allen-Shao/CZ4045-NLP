c072928e4e22876c8faeb109556937c6aafd9a8c
ZWLori <lori961120@gmail.com>
2017-10-31 22:54:45 +0800

    Add stemming and tokenizer python file

--- src/stemming.py
+++ src/stemming.py
@@ -1,9 +1,12 @@
-import csv
 import operator
 from re import match as re_match
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+import pandas as pd
+import sys
+import argparse
+import os
 
 '''
 1. From the dataset, identify the top-20 most frequent words
@@ -12,69 +15,79 @@ from nltk.tokenize import RegexpTokenizer
 2. For each of the top-20 most frequent stems,
 list their original words from which the stem is reached
 '''
+# input arguments: selected_answer.csv   selected_questions.csv
+parser = argparse.ArgumentParser()
+parser.add_argument("selected_questions", help="path of selected_questions.csv")
+parser.add_argument("selected_answers", help="path of selected_answer.csv")
+args = parser.parse_args()
+
+
+
+ans_path = args.selected_answers
+qns_path = args.selected_questions
 
 tokenizer = RegexpTokenizer(r'\w+')
 stemmer = SnowballStemmer("english")
 
-try:
-    with open('../processed_data/answers.csv', 'r') as ansfile:
-        reader = csv.reader(ansfile, delimiter=',')
-        # skip header
-        next(reader, None)
-        i = 0
-        word_frequency = {}
-        stem_frequency = {}
-        stem_origin = {}
-        stems = []
-        for answer in reader:
-            answer_body = answer[2]
-            # change all to lower case
-            tokens = map(str.lower,tokenizer.tokenize(answer_body))
-            # remove stop words and numbers
-            filtered_tokens = [token for token in tokens 
-                               if token not in stopwords.words('english') 
-                               and re_match(r'[-]?\d[\d,]*[\.]?[\d{2}]*', token) is None]
-            
-            for token in filtered_tokens:
-                # calculate word frequency
-                if token in word_frequency:
-                    word_frequency[token] += 1
-                else:
-                    word_frequency[token] = 1
+print(ans_path)
+print(qns_path)
+ans = pd.read_csv(ans_path)
+qns = pd.read_csv(qns_path)
 
-                stem = stemmer.stem(token)
-                stems.append(stem)
-                # preserve the original word for stem
-                if stem in stem_origin:
-                    stem_origin[stem].add(token)
-                else:
-                    stem_origin[stem] = set([token])
+posts = pd.concat([ans,qns])['Body']
 
-            # find the top 20 frequently appearing words
-            top_20_words = [word_f_pair[0] for word_f_pair in 
-                                reversed(sorted(word_frequency.items(), key=operator.itemgetter(1))[-20:])]
+i = 0
+word_frequency = {}
+stem_frequency = {}
+stem_origin = {}
+stems = []
+for post in posts:
+    # change all to lower case
+    tokens = map(str.lower,tokenizer.tokenize(post))
+    # remove stop words and numbers
+    filtered_tokens = [token for token in tokens
+                       if token not in stopwords.words('english')
+                       and re_match(r'[-]?\d[\d,]*[\.]?[\d{2}]*', token) is None]
 
-            # find the top 20 frequently apearing stems
-            for stem in stems:
-                if stem in stem_frequency:
-                    stem_frequency[stem] += 1
-                else:
-                    stem_frequency[stem] = 1
-            top_20_stems = [stem_f_pair[0] for stem_f_pair in 
-                                reversed(sorted(stem_frequency.items(), key=operator.itemgetter(1))[-20:])]
+    for token in filtered_tokens:
+        # calculate word frequency
+        if token in word_frequency:
+            word_frequency[token] += 1
+        else:
+            word_frequency[token] = 1
 
-            # for each stem in top-20 list, get the original words
-            top_20_stem_origins = {stem: stem_origin[stem] for stem in top_20_stems}
-    
-except KeyboardInterrupt:
-    print("keyboard interrupt")
+        stem = stemmer.stem(token)
+        stems.append(stem)
+        # preserve the original word for stem
+        if stem in stem_origin:
+            stem_origin[stem].add(token)
+        else:
+            stem_origin[stem] = set([token])
 
-finally:
-    print("check result")
-    print("top_20_words")
-    print(top_20_words)
-    print("top_20_stems")
-    print(top_20_stems)
-    print("top_20_stem_origins")
-    print(top_20_stem_origins)
-    
+    # find the top 20 frequently appearing words
+    top_20_words = [word_f_pair[0] for word_f_pair in
+                        reversed(sorted(word_frequency.items(), key=operator.itemgetter(1))[-20:])]
+
+    # find the top 20 frequently apearing stems
+    for stem in stems:
+        if stem in stem_frequency:
+            stem_frequency[stem] += 1
+        else:
+            stem_frequency[stem] = 1
+    top_20_stems = [stem_f_pair[0] for stem_f_pair in
+                        reversed(sorted(stem_frequency.items(), key=operator.itemgetter(1))[-20:])]
+
+    # for each stem in top-20 list, get the original words
+    top_20_stem_origins = {stem: stem_origin[stem] for stem in top_20_stems}
+
+writer = open("./stemming_result.txt", "w")
+writer.write("top_20_words\n")
+writer.write(str(top_20_words))
+writer.write("\n")
+writer.write("top_20_stems\n")
+writer.write(str(top_20_stems))
+writer.write("\n")
+writer.write("top_20_stem_origins\n")
+writer.write(str(top_20_stem_origins))
+writer.close()
+print("The stemming result is stored as stemming_result.txt")
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,21 +2,41 @@ from nltk.tokenize import RegexpTokenizer
 import pandas as pd
 import nltk
 import re
-import numpy as np
+import string
+from nltk import pos_tag, word_tokenize
+import argparse
+import os
+
 
 def tokenize_code(code, dic, post):
     code_lines = list(filter(None, code.split('\n')))
     for line in code_lines:
         # store line info then append to 'post' array
         line_info = [line]
-        tokens = tokenizer.tokenize(line)
+        tokens = code_tokenizer.tokenize(line)
+        # remove function definition from matching the case of function call
+        fun_def = False
+        for t in tokens:
+            if re.match(function_call, t) and not re.match(r'(?:\w+\.\w+\s*\(.*\))',t):
+                # if there is a { symbol after a function
+                # re-tokenize the line
+                if t != tokens[-1] and '{' in tokens[tokens.index(t)+1:]:
+                    fun_def = True
+                    break
+        if fun_def:
+            temp_patterns = patterns =  keywords + '|' + string_literal + '|' + comments + '|'\
+                + directory + '|' + identifier + '|' + floating_literal + '|' \
+                + hex_literal + '|' + operators + '|' + octal_literal + '|' \
+                + decimal_literal
+            temp_code_tokenizer = RegexpTokenizer(temp_patterns)
+            tokens = temp_code_tokenizer.tokenize(line)
         line_info.append(tokens)
         dic['tokens'] += (tokens)
-        if tokens and re.match(comments, tokens[0]):
-            t = tokens[0]
+        if tokens and re.match(comments, tokens[-1]):
+            t = tokens[-1]
             t = t.replace('//', '')
             # tokenize comments as natural language
-            t = natural_lang_tokenizer.tokenize(t)
+            t = tokenize_natural_language(t)
             last = line_info.pop()
             last.pop()
             line_info.append(last + ['//'] + t)
@@ -26,21 +46,130 @@ def tokenize_code(code, dic, post):
         post.append(line_info)
     return dic, post
 
+def split_punc(token):
+    if token == '...':
+        return [token, token]
+    punc = string.punctuation.replace('/', '')
+    punctuation_mark = r"[{}]".format(punc) # create the pattern
+    left = '<{[('
+    right = '>}])'
+    if re.match(punctuation_mark, token[0]) and not re.match(punctuation_mark, token[-1]):
+#         print("first char is punctuation mark")
+        token_lst = []
+        iter_token = iter(range(len(token)))
+        for i in iter_token:
+            if token[i] in punc:
+                if token[i] in left:
+                    corresponding_right = right[left.index(token[i])]
+                    if corresponding_right in token:
+                        token_lst.append(token[i:])
+                        token_lst.insert(0, token[i:])
+                        break
+                token_lst.append(token[i])
+            else:
+                token_lst.append(token[i:])
+                # copy the word token in front for later processing
+                token_lst.insert(0, token[i:])
+                break
+        return token_lst
+    elif re.match(punctuation_mark, token[-1]) and not re.match(punctuation_mark, token[0]):
+#         print("last char is punctuation mark")
+        token_lst = []
+        iter_token = iter(range(len(token)-1, -1, -1))
+        for i in iter_token:
+            if token[i] in punc:
+
+                if token[i] in right:
+                    corresponding_left = left[right.index(token[i])]
+                    if corresponding_left in token:
+                        token_lst.insert(0, token[:i+1])
+                        token_lst.insert(0, token[:i+1])
+                        break
+                token_lst.insert(0, token[i])
+            else:
+                token_lst.insert(0, token[:i+1])
+                # copy the word token in front for later processing
+                token_lst.insert(0, token[:i+1])
+                break
+
+        return token_lst
+    else:
+        return [token, token]
+
+def tokenize_natural_language(txt):
+#     re_entity = r'(?:[A-Z][A-Za-z]*\s)*[A-Z][A-Za-z]*'
+
+#     entities = RegexpTokenizer(re_entity).tokenize(txt)
+#     name_entities = []
+#     for entity in entities:
+#         if " " not in entity:
+#             break
+#         satisfier = True
+#         pos = nltk.pos_tag(entity.split(" "))[0][1]:
+#         if pos == 'DT' or pos == 'PRP'or pos == 'CC' or pos == 'WRB' or pos == 'IN':
+#             satisfier = False
+#             break
+#         if satisfier:
+#             name_entities.append(entity)
+
+#     if name_entities:
+#         print(name_entities)
+#     dic = {entity:entity.split(" ") for entity in name_entities}
+#     entity_tokens = [item for sublist in dic.values() for item in sublist]
+    space_tokenizer = RegexpTokenizer(r'\S+')
+    tokens = space_tokenizer.tokenize(txt)
+
+    new_token_list = []
+#     find_entity = False
+    iter_tokens = iter(tokens)
+    for token in iter_tokens:
+#         for key, val in dic.items():
+#             index = tokens.index(token)
+#             if token in val and (index+len(val)) < len(tokens):
+#                 reconstruct = " ".join([tokens[t] for t in range(index, index + len(val))])
+#                 splited = split_punc(reconstruct)
+#                 if splited[0] == key:
+#                     new_token_list += splited[1:]
+#                     if len(val) != 1:
+#                         for n in range(len(val)-1):
+#                             next(iter_tokens)
+#                     find_entity = True
+#                     break
+#         if find_entity:
+#             find_entity = False
+#             continue
+
+        new_token_list += split_punc(token)[1:]
+
+    # split abbreviation
+    abbreviation = r'(?:n\'t$)|(?:\'s$)|(?:\'m$)|(?:\'ve$)|(?:\'re$)|(?:\'ll$)|(?:\'d$)'
+    old_tokens = new_token_list[:]
+    match_count = 0
+    for i, t in enumerate(old_tokens):
+        if re.search(abbreviation, t):
+            m = re.search(abbreviation, t)
+            word_root = t[:m.span()[0]]
+            suffix = t[m.span()[0]:m.span()[1]]
+            new_token_list[i+match_count] = word_root
+            new_token_list.insert(i+match_count+1, suffix)
+            match_count += 1
+
+    return new_token_list
+
 def _tokenizer(post_df, code_df, store_directory, store_each_post = False, process_amount = None):
     tokenization=[]
     count = 0
     for post_number,p_id in enumerate(post_df.index.values):
         no_match = False
         body = post_df.loc[p_id]['Body']
+        # dic is used to store the overall info for one post
+        dic = {'text' : body, 'tokens' : []}
         # separate code and natural language, store into parts
         parts = []
         # 1 for code, 0 for natural language
         code_or_not = []
         # if the body does not have any code block, no need to process
-        if p_id not in code_df.index:
-            parts.append(body)
-            code_or_not=[0]
-        else:
+        if p_id in code_df.index:
             code_blks = code_df.loc[p_id]['Code']
             # check code blk position in a post
             # arrange them in order
@@ -77,43 +206,52 @@ def _tokenizer(post_df, code_df, store_directory, store_each_post = False, proce
 
                 parts = list(filter(None, parts))
                 body = rest[-1]
-            if len(rest) == 2 and rest[1].strip():
+            if keylist and len(rest) == 2 and rest[1].strip():
                 parts.append(rest[1])
                 code_or_not.append(0)
+        # if there is not code block
+        if not parts:
+            parts.append(body)
+            code_or_not=[0]
         # post is used to store line by line info
         post = []
-        # dic is used to store the overall info for one post
-        dic = {'text' : body, 'tokens' : []}
         for index, value in enumerate(parts):
             if code_or_not[index]:
                 dic, post = tokenize_code(value, dic, post)
             else:
-                token = natural_lang_tokenizer.tokenize(value)
+                token = tokenize_natural_language(value)
                 post.append([value, token])
                 dic['tokens'] += token
-
         # df for each post, do line by line annotation
         if store_each_post:
             post_tokenization_df = pd.DataFrame(post, columns = ['text', 'token'])
-            post_tokenization_df.to_csv(store_directory + str(post_number)+'.csv')
+            post_tokenization_df.to_csv(os.path.join(store_directory, str(post_number) + ".csv"))
         tokenization.append([p_id, dic['text'], dic['tokens']])
         count +=1
         if process_amount and count >= process_amount:
             break
     # df for all posts
     results = pd.DataFrame(tokenization, columns = ['post_id', 'text', 'tokens'])
-    results.to_csv(store_directory + 'overall_results.csv')
+    results.to_csv(os.path.join(store_directory + 'overall_results.csv'))
     return results
 
-ALL_codes = pd.read_csv("../processed_data/code.csv")
+# main
+parser = argparse.ArgumentParser()
+parser.add_argument("code", help="path of code.csv")
+parser.add_argument("selected_questions", help="path of selected_questions.csv")
+parser.add_argument("selected_answers", help="path of selected_answer.csv")
+parser.add_argument("ground_truth", help="path of 150_ground_truth_post.csv")
+args = parser.parse_args()
+
+ALL_codes = pd.read_csv(args.code)
 ALL_codes.set_index('PostId', inplace=True)
 
-selected_qns = pd.read_csv('../processed_data/selected_questions.csv')
-selected_ans = pd.read_csv('../processed_data/selected_answer.csv')
+selected_qns = pd.read_csv(args.selected_questions)
+selected_ans = pd.read_csv(args.selected_answers)
 selected_post = pd.concat([selected_ans, selected_qns])[['PostId', 'Body']]
 selected_post.set_index('PostId', inplace=True)
 
-ground_truth_post = pd.read_csv('../processed_data/150_ground_truth_post.csv')
+ground_truth_post = pd.read_csv(args.ground_truth)
 ground_truth_post.set_index('PostId', inplace=True)
 ground_truth_post_ids = ground_truth_post.index.values
 
@@ -137,29 +275,34 @@ keywords = r'''break|default|func|interface|select|case|defer
 # operators and punctuation
 operators = r'[%/\+\-\*\,;\$><!:\.\|&\^=\(\)\[\]\{\}]+'
 
+# special symbol
+special = r'[#]'
+
 decimal_literal = r'\d+i?'
 octal_literal = r'0[1-7]*'
-hex_literal = r'0[xX][1-9a-fA-F]+'
-floating_literal = r''' \d+\.\d*(?:[eE][+-]\d+)?i?
-                | \d+[eE][+-]\d+i?
-                | \.\d+(?:[eE][+-]\d+)?i?
-                '''
+hex_literal = r'0[xX][0-9a-fA-F]+'
+floating_literal = r'''(?:\d+\.\d*(?:[eE][+-]\d+)?i?)|(?:\d+[eE][+-]\d+i?)|(?:\.\d+(?:[eE][+-]\d+)?i?)'''
 string_literal = r'''(?:\"\s*.*?\n?\")|(?:\'\s*.*?\n?\')'''
 
 comments = r'//.*$'
-function_call = r'\w+\.\w+\s*\(.*\)'
+function_call = r'(?:\w+\.\w+\s*\(.*\))|(?:\w+\s*\(.*\))'
 directory = r'/?\w+/(?:.+/)*\S+/?'
 
-patterns =  keywords + '|' + function_call + '|' + string_literal + '|' + comments + '|'\
-        + directory + '|' + identifier + '|' + operators + '|' \
-        + hex_literal + '|' + floating_literal + '|' + octal_literal + '|' \
+patterns = special + '|' + keywords + '|' + function_call + '|' + string_literal + '|' + comments + '|'\
+        + directory + '|' + identifier + '|' + floating_literal + '|' \
+        + hex_literal + '|' + operators + '|' + octal_literal + '|' \
         + decimal_literal
 
-tokenizer = RegexpTokenizer(patterns)
-natural_lang_tokenizer = RegexpTokenizer(r'\S+')
+code_tokenizer = RegexpTokenizer(patterns)
 
+if not os.path.exists('./processed_gt_data/'):
+    os.makedirs('./processed_gt_data/')
 # tokenize all ground truth posts
-gt_results = _tokenizer(ground_truth_post, ground_truth_code_blk, '../processed_data/tokenizer_result/gt_results/', True, 100)
+gt_results = _tokenizer(ground_truth_post, ground_truth_code_blk, './processed_gt_data/', True, 100)
+print("The ground truth tokenization result is under ./processed_gt_data/")
 
+if not os.path.exists('./processed_all_data/'):
+    os.makedirs('./processed_all_data/')
 # tokenize all posts from 700 threads
-all_results = _tokenizer(selected_post, ALL_codes , '../processed_data/tokenizer_result/others/', False)
+all_results = _tokenizer(selected_post, ALL_codes , './processed_all_data/', False)
+print("The overall tokenization result is under ./processed_all_data/")
