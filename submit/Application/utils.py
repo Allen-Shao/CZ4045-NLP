7d646a09c3e3df8e7d626fc011124c87a7d91137
iamqingmei <iamqingmei@gmail.com>
2017-11-01 22:22:58 +0800

    add application folder in submit, update readme for application files

--- submit/Application/utils.py
+++ submit/Application/utils.py
@@ -5,6 +5,7 @@ from collections import Counter
 from six.moves import cPickle
 import numpy as np
 
+
 class TextLoader():
     def __init__(self, data_dir, batch_size, seq_length, encoding=None):
         self.data_dir = data_dir
@@ -36,7 +37,7 @@ class TextLoader():
         with open(vocab_file, 'wb') as f:
             cPickle.dump(self.words, f)
 
-        #The same operation like this [self.vocab[word] for word in x_text]
+        # The same operation like this [self.vocab[word] for word in x_text]
         # index of words as our basic data
         self.tensor = np.array(list(map(self.vocab.get, x_text)))
         # Save the data to data.npy
@@ -44,7 +45,7 @@ class TextLoader():
 
         self.num_batches = int(self.tensor.size / (self.batch_size *
                                                    self.seq_length))
-        if self.num_batches==0:
+        if self.num_batches == 0:
             assert False, "Not enough data. Make seq_length and batch_size small."
 
         self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
@@ -58,7 +59,6 @@ class TextLoader():
 
         self.reset_batch_pointer()
 
-
     def next_batch(self):
         x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
         self.pointer += 1
@@ -66,3 +66,49 @@ class TextLoader():
 
     def reset_batch_pointer(self):
         self.pointer = 0
+
+
+# method for generating text
+def generate_text(model, length, vocab_size, ix_to_char):
+    # starting with random character
+    ix = [np.random.randint(vocab_size)]
+    y_char = [ix_to_char[ix[-1]]]
+    X = np.zeros((1, length, vocab_size))
+    for i in range(length):
+        # appending the last predicted character to sequence
+        X[0, i, :][ix[-1]] = 1
+        print(ix_to_char[ix[-1]], end="")
+        ix = np.argmax(model.predict(X[:, :i + 1, :])[0], 1)
+        y_char.append(ix_to_char[ix[-1]])
+    return ('').join(y_char)
+
+
+# method for preparing the training data
+def load_data(data_dir, seq_length):
+    data = open(data_dir, 'r', encoding='utf-8').read()
+    chars = list(set(data))
+    VOCAB_SIZE = len(chars)
+
+    print('Data length: {} characters'.format(len(data)))
+    print('Vocabulary size: {} characters'.format(VOCAB_SIZE))
+
+    ix_to_char = {ix: char for ix, char in enumerate(chars)}
+    char_to_ix = {char: ix for ix, char in enumerate(chars)}
+
+    X = np.zeros((int((len(data) / seq_length)), seq_length, VOCAB_SIZE))
+    y = np.zeros((int((len(data) / seq_length)), seq_length, VOCAB_SIZE))
+    for i in range(0, int(len(data) / seq_length)):
+        X_sequence = data[i * seq_length:(i + 1) * seq_length]
+        X_sequence_ix = [char_to_ix[value] for value in X_sequence]
+        input_sequence = np.zeros((seq_length, VOCAB_SIZE))
+        for j in range(seq_length):
+            input_sequence[j][X_sequence_ix[j]] = 1.
+            X[i] = input_sequence
+
+        y_sequence = data[i * seq_length + 1:(i + 1) * seq_length + 1]
+        y_sequence_ix = [char_to_ix[value] for value in y_sequence]
+        target_sequence = np.zeros((seq_length, VOCAB_SIZE))
+        for j in range(seq_length):
+            target_sequence[j][y_sequence_ix[j]] = 1.
+            y[i] = target_sequence
+    return X, y, VOCAB_SIZE, ix_to_char
